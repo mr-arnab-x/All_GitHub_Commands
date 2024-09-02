ed82d621a12a2f1592bf7c4cb19613416a4f6e80
mr-arnab-x <santraarnab241@gmail.com>
2024-09-02 15:38:12 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,3 @@
+// Example JavaScript file for additional functionality
+// Currently, it does not have any functionality, but can be used for future enhancements
+console.log("JavaScript file loaded.");
